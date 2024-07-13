a3e0f9b878f4bf1f682877157ec5471f927d5bb1
Sunny Tamang <sunnytamang@Sunnys-MacBook-Pro.local>
2024-07-13 19:59:01 +0530

    print_selected

--- lua/select-undo/init.lua
+++ lua/select-undo/init.lua
@@ -2,12 +2,20 @@ local M = {}
 
 function M.setup(opts)
    opts = opts or {}
-  vim.keymap.set("n", "<C-s>", function()
-    local line = vim.api.nvim_get_current_line()
-    print("Selected line: " .. line)
+  --vim.keymap.set("n", "<C-s>", function()
+  --  local line = vim.api.nvim_get_current_line()
+   -- print("Selected line: " .. line)
+  --end
+  --)
+  vim.keymap.set("n", "<C-s", function()
+    local start_line, end_line = vim.fn.line("'<"), vim.fn.line("'>")
+    for line_nr = start_line, end_line do 
+      local line = vim.fn.getline(line_nr)
+      print('Selected_line ' .. line_nr .. ":" .. line)
+    end
   end
-  
   )
+
   vim.keymap.set("n","<C-s>u",function()
     local line_nr = vim.api.nvim_win_get_cursor(0)[1]
     vim.cmd(line_nr .. 'undo')
