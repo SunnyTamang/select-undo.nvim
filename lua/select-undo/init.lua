6ee7cfe73676a9ae7ff35d63d941f25f7528499b
Sunny Tamang <sunnytamang@Sunnys-MacBook-Pro.local>
2024-07-13 19:51:27 +0530

    get the linenumber and undo

--- lua/select-undo/init.lua
+++ lua/select-undo/init.lua
@@ -2,9 +2,15 @@ local M = {}
 
 function M.setup(opts)
    opts = opts or {}
-  vim.keymap.set("n", "<C-s>u", function()
+  vim.keymap.set("n", "<C-s>", function()
     local line = vim.api.nvim_get_current_line()
     print("Selected line: " .. line)
+  end
+  
+  )
+  vim.keymap.set("n","<C-s>u",function()
+    local line_nr = vim.api.nvim_win_get_cursor(0[1])
+    vim.cmd(line_nr .. 'undo')
   end
   )
      
